1bac89215e39c184801172a758d632302a9296a9
ShubinGeorge <shubin.ga@phystech.edu>
2019-02-19 21:44:41 +0300

    Not finished homework

---
+++ Homework2/Homework2/matrix.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <istream>
+#include <ostream>
+
+
+
+class Matrix
+{
+    friend std::istream& operator>>(std::istream& input_stream, Matrix& matrix);
+	friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+
+public: 
+    Matrix(const size_t column_number, const size_t line_number);
+
+    size_t GetHeight() const;
+    size_t GetWidth() const;
+
+    int GetValue(const size_t height_index, const size_t width_index) const;
+    void SetValue(const size_t height_index, const size_t width_index,
+                  const int value);
+
+    Matrix Transpose() const;
+
+
+    Matrix operator*(const Matrix& rhs);
+
+private:
+    std::vector<std::vector<int>> data_;
+};
+
+
+std::istream& operator>>(std::istream& input_stream, Matrix& matrix);
+std::ostream& operator<<(std::ostream& output_stream, const Matrix& matrix);
+
